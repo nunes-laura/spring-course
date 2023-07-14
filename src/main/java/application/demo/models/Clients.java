944927f76f67d09a56360d7a980eecdbd7fbcb9d
Laura Nunes <lauranunes2508@gmail.com>
2023-07-14 15:49:26 -0300

    Project + CRUD created

---
+++ src/main/java/application/demo/models/Clients.java
@@ -0,0 +1,86 @@
+package application.demo.models;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_clients")
+public class Clients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "gender")
+    private String gender;
+
+    public Clients() {}
+
+    public Clients(UUID id, String firstName, String lastName, String address, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.gender = gender;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clients clients = (Clients) o;
+        return Objects.equals(id, clients.id) && Objects.equals(firstName, clients.firstName) && Objects.equals(lastName, clients.lastName) && Objects.equals(address, clients.address) && Objects.equals(gender, clients.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
