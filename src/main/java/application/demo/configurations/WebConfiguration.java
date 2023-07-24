9d3cfa387ecd2550c28df58c6cb7e8943a204d66
Laura Nunes <lauranunes2508@gmail.com>
2023-07-24 12:36:14 -0300

    Content Negotiation - Query Parameter

---
+++ src/main/java/application/demo/configurations/WebConfiguration.java
@@ -0,0 +1,29 @@
+package application.demo.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+
+        //QUERY PARAM: http://localhost:8080/clients?mediaType=xml
+
+        configurer.favorParameter(true)
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+
+
+    }
+
+
+
+}
--- src/main/java/application/demo/controllers/ClientsController.java
+++ src/main/java/application/demo/controllers/ClientsController.java
@@ -6,6 +6,7 @@ import application.demo.services.ClientsService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,22 +21,31 @@ public class ClientsController {
     @Autowired
     private ClientsService service;
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE })
+
     public ResponseEntity<List<ClientDTO>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body( service.findAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+
     public ResponseEntity<ClientDTO> findById(@PathVariable UUID id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
-    @PostMapping("/save")
+    @PostMapping(value = "/save",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+
     public ResponseEntity<ClientDTO> create(@Valid @RequestBody ClientDTO c){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(c));
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping(value = "/update/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+
     public ResponseEntity<ClientDTO> update (@PathVariable UUID id,@Valid @RequestBody Clients c){
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.update(id, c));
 
@@ -44,6 +54,7 @@ public class ClientsController {
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> delete(@PathVariable UUID id){
             service.delete(id);
+
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 
     }
