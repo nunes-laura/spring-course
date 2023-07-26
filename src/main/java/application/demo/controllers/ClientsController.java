f61958e4cc80f1ce031630967df3b1a686fc4dba
Laura Nunes <lauranunes2508@gmail.com>
2023-07-26 10:47:38 -0300

    YAML Serialization

--- src/main/java/application/demo/controllers/ClientsController.java
+++ src/main/java/application/demo/controllers/ClientsController.java
@@ -3,16 +3,15 @@ package application.demo.controllers;
 import application.demo.dtos.ClientDTO;
 import application.demo.models.Clients;
 import application.demo.services.ClientsService;
+import application.demo.util.MediaType;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clients")
@@ -21,30 +20,30 @@ public class ClientsController {
     @Autowired
     private ClientsService service;
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE })
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 
     public ResponseEntity<List<ClientDTO>> findAll(){
         return ResponseEntity.status(HttpStatus.OK).body( service.findAll());
     }
 
     @GetMapping(value = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 
     public ResponseEntity<ClientDTO> findById(@PathVariable UUID id){
         return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
     }
 
     @PostMapping(value = "/save",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML },
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 
     public ResponseEntity<ClientDTO> create(@Valid @RequestBody ClientDTO c){
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(c));
     }
 
     @PutMapping(value = "/update/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 
     public ResponseEntity<ClientDTO> update (@PathVariable UUID id,@Valid @RequestBody Clients c){
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.update(id, c));
